82a2be94944967627c78e83a7e365e8704d6e8c9
Jared Christian Marcos Gatchalian <jmgatchalian@student.apc.edu.ph>
2026-01-27 11:01:15 +0800

    ahahaha

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,10 +1,13 @@
-// main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
+import Country from './components/Country.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
+
 const app = createApp(App)
-
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
-app.component('personal-profile', PersonalProfile)
-app.component('comment-form', CommentForm) // Move this up
-app.component('comment', Comment)           // Move this up
-
-app.mount('#app') // This MUST be last
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
+app.mount('#app')
